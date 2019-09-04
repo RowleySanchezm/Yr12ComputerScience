e56dec8d5d9dfee04b4422c1a923e7f1784b278c
Max RS <max.rowley.sanchez@gmail.com>
2019-09-04 13:03:50 +0100

    Update Python paint algorithm.py

--- Python paint algorithm.py
+++ Python paint algorithm.py
@@ -8,6 +8,6 @@ print(length)
 width = input("Enter width in metres ")
 width = int(width)
 print(width)
-#Assuming that the floor does not need to be painted and room is a cuboid
+#Assuming that the floor does not need to be painted and room is a cuboid with no windows
 PaintNeeded = ((height*length)*2) + ((height*width)*2) + (width*length)
 print(PaintNeeded, "m^2") 
