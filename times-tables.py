dc98a508f82f1b204afb7b714b946d31200f06d1
Max RS <max.rowley.sanchez@gmail.com>
2019-09-13 15:26:58 +0100

    fixing of the verification

--- times-tables.py
+++ times-tables.py
@@ -1,35 +1,45 @@
 #Times table
 
-while True:
-    print("Welcome to the times table calculator.")
-    table = -1
-    while table < 1 or table > 20:
-        table = int(input("What table [1-20]?"))
-    #end while
-    table2 = -1
-    while table2 < 1 or table2 > 20:
-        table2 = int(input("Please confirm which table you would like to select?"))
-    #end while
-    if table == table2:
-        print("You have selected the", table2, "times table.")
-    else:
-        print("Your selected tables do not match, please try again.")
-    #end if
+verified = False
 
-    rows = -1
-    while rows < 1 or rows > 20:
-        rows = int(input("How many rows [1-20]?"))
+while True:
+        print("Welcome to the times table calculator.")
+    while not verified: 
+        table = -1
+        while table < 1 or table > 20:
+            table = int(input("What table [1-20]?"))
+        #end while
+        table2 = -1
+        while table2 < 1 or table2 > 20:
+            table2 = int(input("Please confirm which table you would like to select?"))
+        #end while
+        if table == table2:
+            print("You have selected the", table2, "times table.")
+            verified = True
+        else:
+            print("Your selected tables do not match, please try again.")
+        #end if
     #end while
-    rows2 = -1
-    while rows2 < 1 or rows2 > 20:
-        rows2 = int(input("Please confirm how many rows you would like to select?"))
+            
+    verified = False
+    
+    while not verified:
+        rows = -1
+        while rows < 1 or rows > 20:
+            rows = int(input("How many rows [1-20]?"))
+        #end while
+        rows2 = -1
+        while rows2 < 1 or rows2 > 20:
+            rows2 = int(input("Please confirm how many rows you would like to select?"))
+        #end while
+        if rows == rows2:
+            print("You have selected", rows2, "rows.")
+            verified = True
+        else:
+            print("The number of selected rows do not match, please try again.")
+        #end if
     #end while
-    if rows == rows2:
-        print("You have selected", rows2, "rows.")
-    else:
-        print("The number of selected rows do not match, please try again.")
-    #end if
-        
+            
     for item in range(1, rows2+1):
         print (item * table2)
     #next
