38f8b9c673102a7c044e68cce2b90b63df3e9bd7
RowleySanchezm <54801526+RowleySanchezm@users.noreply.github.com>
2019-09-19 19:02:57 +0100

    The rest of the response to coding feedback

--- Python practice/Python paint algorithm.py
+++ Python practice/Python paint algorithm.py
@@ -3,30 +3,24 @@ units = input("Please select one of the following; 1: Imperial, 2: Metric ")
 
 if units == 1:
     print("Calculation for the amount of paint that a cuboid room needs:")
-    height = input("Enter height in feet ")
-    height = float(height)
+    height = float(input("Enter height in feet "))
     print(height)
-    length = input("Enter length in feet ")
-    length = float(length)
+    length = float(input("Enter length in feet "))
     print(length)
-    width = input("Enter width in feet ")
-    width = float(width)
+    width = float(input("Enter width in feet "))
     print(width)
 elif units == 2:
-    ### SRC - Did you need to repeat all this code? Can you think of another way?
     print("Calculation for the amount of paint that a cuboid room needs:")
-    height = input("Enter height in metres ")
-    height = float(height)
+    height = float(input("Enter height in metres "))
     print(height)
-    length = input("Enter length in metres ")
-    length = float(length)
+    length = float(input("Enter length in metres "))
     print(length)
-    width = input("Enter width in metres ")
-    width = float(width)
+    width = float(input("Enter width in metres "))
     print(width)
 else:
     print("invalid input")
 #End If
+    
 #Assuming that the floor does not need to be painted and room is a cuboid with no windows
 PaintNeeded = ((height*length)*2) + ((height*width)*2) + (width*length)
 print(PaintNeeded, "m^2") 
--- Python practice/times-tables.py
+++ Python practice/times-tables.py
@@ -3,7 +3,7 @@
 verified = False
 
 while True:
-        print("Welcome to the times table calculator.") ### SRC - no need to indent twice
+        print("Welcome to the times table calculator.")
     while not verified: 
         table = -1
         while table < 1 or table > 20:
