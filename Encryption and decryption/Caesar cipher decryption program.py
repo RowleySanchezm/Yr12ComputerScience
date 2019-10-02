7d0fccb31767ab70ce85577901497e7adb116668
RowleySanchezm <54801526+RowleySanchezm@users.noreply.github.com>
2019-10-02 19:57:55 +0100

    Create Caesar cipher decryption program.py
    decryption program

---
+++ Encryption and decryption/Caesar cipher decryption program.py
@@ -0,0 +1,19 @@
+#Caesar Cipher decryption program
+
+def decrypt(cipher_text,s): 
+    result = "" 
+    for i in range(len(cipher_text)): 
+        char = cipher_text[i]
+    #Next i
+        if (char.isupper()): 
+            result += chr((ord(char) - s-65) % 26 + 65)
+        else: 
+            result += chr((ord(char) - s - 97) % 26 + 97)
+        #End if
+    return result
+#End function
+
+Cipher_text = input("Please enter the cipher text: ")
+shift = int(input("Enter shift: "))
+
+print(decrypt(Cipher_text,shift))
