f2f808487e2c77fc02f818b42d71c6ba3f742da5
Max RS <max.rowley.sanchez@gmail.com>
2020-03-01 18:48:30 +0000

    initial commitment for project

---
+++ Alevel project/Realm defender.py
@@ -0,0 +1,69 @@
+import pygame
+import os
+
+class Game:
+    def __init__(self):
+        self.width = 1000
+        self.height = 700
+        self.win = pygame.display.set_mode((self.width, self.height))
+        self.enemies = []
+        self.towers = []
+        self.lives = 10
+        self.money = 100
+        self.background = pygame.image.load(os.path.join("Game_images","background1.png"))
+
+    def run(self):
+        run = true
+        clock = pygame.time.Clock()
+        while run:
+            clock.tick(60)
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    run = False
+
+            self.draw()
+            
+
+        pygame.quit()
+
+    def draw(self):
+        self.win.blit(self.background, (0,0))
+        pygame.display.update()
+
+class Enemy:
+    imgs = []
+    
+    def __init__(self, x, y, width, height):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.animation_count = 0
+        self.health = 1
+        self.path = []
+        self.img = None
+
+    def draw(self, win):
+        self.animation_count += 1
+        self.img = self.imgs[self.animation_count]
+        
+        if self.animation_count >= len(self.imgs):
+            self.animation_count = 0
+            
+        win.blit(self.img, (self.x, self.y))
+        self.move()
+
+    def collide(self, X, Y):
+        if X <= self.x + self.width and X >= self.x:
+            if Y <= self.y + self.height and Y >= self.y:
+                return True
+        return False
+
+    def move(self):
+
+        pass
+
+    def hit(self):
+        self.health -= 1
+        if self.health <= 0:
+            return True
