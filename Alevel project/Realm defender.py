96d667db31111f2ed218f52d743c4443a53c6709
Max RS <max.rowley.sanchez@gmail.com>
2020-03-01 21:03:07 +0000

    Enemy should now move from point to point

--- Alevel project/Realm defender.py
+++ Alevel project/Realm defender.py
@@ -1,5 +1,6 @@
 import pygame
 import os
+import math
 
 class Game:
     def __init__(self):
@@ -41,8 +42,12 @@ class Enemy:
         self.height = height
         self.animation_count = 0
         self.health = 1
+        self.velocity = 3
         self.path = [(1078, 142), (485, 144), (418, 162), (394, 197), (379, 240), (380, 282), (401, 328), (438, 355), (481, 370), (528, 390), (554, 428), (563, 461), (572, 500), (586, 546), (622, 575), (667, 583), (724, 582), (1026, 584), (1095, 582)]
         self.img = None
+        self.path_pos = 0
+        self.move_count = 0
+        self.distance = 0
 
     def draw(self, win):
         self.animation_count += 1
@@ -61,8 +66,30 @@ class Enemy:
         return False
 
     def move(self):
+        x1, y2 = self.path[self.path_pos]
+        if self.path_pos + 1 >= len(self.path):
+            x2, y2 = (1120, 582)
+        else:
+            x2, y2 = self.path[self.path_pos+1]
 
-        pass
+        move_distance = math.sqrt((x2-x1)**2 + (y2-y1)**2)
+        
+        self.move_count += 1
+        direction = (x2-x1, y2-y1)
+        
+        move_x, move_y = (self.x + direction[0] * self.move_count, self.y + direction[1] * self.move_count)
+        self.distance += math.sqrt((move_x-x1)**2 + (move_y-y1)**2)
+
+        if self.distance >= move_distance:
+            self.distance = 0
+            self.move_count = 0
+            self.path_pos += 1
+        
+
+        self.x = move_x
+        self.y = move_ y
+
+            
 
     def hit(self):
         self.health -= 1
