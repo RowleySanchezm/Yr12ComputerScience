9ffc881fc52f6c2954a441a50afb856de8abdd7f
Max RS <max.rowley.sanchez@gmail.com>
2020-03-01 19:59:13 +0000

    Hard path map
    Using the mouse positions to create a fixed path for the enemy to follow

--- Alevel project/Realm defender.py
+++ Alevel project/Realm defender.py
@@ -11,15 +11,23 @@ class Game:
         self.lives = 10
         self.money = 100
         self.background = pygame.image.load(os.path.join("Game_images","background1.png"))
+        self.clicks = []
 
     def run(self):
-        run = true
+        run = True
         clock = pygame.time.Clock()
         while run:
             clock.tick(60)
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     run = False
+
+                pos = pygame.mouse.get_pos()
+
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    self.clicks.append(pos)
+                    print(pos)
+                    
 
             self.draw()
             
@@ -28,6 +36,8 @@ class Game:
 
     def draw(self):
         self.win.blit(self.background, (0,0))
+        for p in self.clicks:
+            pygame.draw.circle(self.win, (255,0,0), (p[0], p[1]), 5, 0)
         pygame.display.update()
 
 class Enemy:
@@ -67,3 +77,7 @@ class Enemy:
         self.health -= 1
         if self.health <= 0:
             return True
+
+
+g = Game()
+g.run()
