6baffc4a9956fe0ff2126b76b82c9ea9c0b43f51
Max RS <max.rowley.sanchez@gmail.com>
2020-03-23 17:34:28 +0000

    Menu update
    First iteration of the menu. When it runs its fine but as soon as you select anything it crashes.
    Traceback (most recent call last):
      File "/Users/Max/Documents/GitHub/Yr12ComputerScience/Alevel project/Realm defender.py", line 518, in <module>
        g.run()
      File "/Users/Max/Documents/GitHub/Yr12ComputerScience/Alevel project/Realm defender.py", line 82, in run
        self.draw()
      File "/Users/Max/Documents/GitHub/Yr12ComputerScience/Alevel project/Realm defender.py", line 92, in draw
        towers.draw(self.win)
      File "/Users/Max/Documents/GitHub/Yr12ComputerScience/Alevel project/Realm defender.py", line 388, in draw
        super().draw(win)
      File "/Users/Max/Documents/GitHub/Yr12ComputerScience/Alevel project/Realm defender.py", line 330, in draw
        self.menu.draw(win)
      File "/Users/Max/Documents/GitHub/Yr12ComputerScience/Alevel project/Realm defender.py", line 154, in draw
        win.blit(self.menu_background, (self.x, self.y))
    AttributeError: 'Menu' object has no attribute 'menu_background'
    This leads me to believe that the problem is to do with my selected variable

--- Alevel project/Realm defender.py
+++ Alevel project/Realm defender.py
@@ -111,18 +111,54 @@ class Game:
 
 
 
-class Menu:
-    def __init__(self,x, y):
+class Button:
+    def __init__(self, x, y, img, name):
+        self.name = name
+        self.img = img
         self.x = x
         self.y = y
-        self.width = 0
-        self.height = 0
+        self.width = self.img.get_width()
+        self.height = self.img.get_height()
+
+    def click(self, X, Y):
+        if X <= self.x + self.width and X >= self.x:
+            if Y <= self.y + self.height and Y >= self.y:
+                return True
+        return False
+
+    def draw(self, win):
+        win.blit(self.img, (self.x, self.y))
+
+
+    
+class Menu:
+    def __init__(self,x, y, img):
+        self.x = x
+        self.y = y
+        self.width = img.get_width()
+        self.height = img.get_height()
         self.item_names = []
         self.imgs = []
+        self.buttons = []
         self.items = 0
+        self.bg = img
 
-    def click(self, x, y):
-        pass
+    def add_button(self, img, name):
+        self.items += 1
+        increment_x = self.width/self.items/2
+        button_x = self.items * increment_x - img.get_width()/2
+        button_y = self.y + self.height/2 - img.get_height()/2
+        self.buttons.append(Button(button_x, button_y, img, name))
+
+    def draw(self, win):
+        win.blit(self.menu_background, (self.x, self.y))
+        for item in self.buttons:
+            item.draw(win)
+    
+    def get_clicked(self, X, Y):
+        for button in self.buttons:
+            if button.click(X, Y):
+                return button.name
 
 
 
@@ -268,6 +304,9 @@ class Swordsman(Enemy):
         self.imgs = Swordsman_imgs[:]
 
 
+
+menu_background = pygame.image.load(os.path.join("Game_images/GameInterface", "menu.png"))
+
 class Tower:
     def __init__(self, x, y):
         self.x = x
@@ -278,13 +317,17 @@ class Tower:
         self.price = [0, 0, 0]
         self.level = 1
         self.selected = False
-        self.menu = None
+        self.menu = Menu(self.x, self.y, menu_background)
         self.tower_imgs = []
         self.damage = 1
 
     def draw(self, win):
         img = self.tower_imgs[self.level-1]
         win.blit(img, (self.x-img.get_width()//2, self.y-img.get_height()//2))
+
+        #Drawing menu
+        if self.selected:
+            self.menu.draw(win)
 
     def draw_radius(self, win):
         if self.selected:
