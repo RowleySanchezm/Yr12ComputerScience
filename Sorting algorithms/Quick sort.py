03b0a89199d0a26f576b7c2dc5b705b926fb240c
Max RS <max.rowley.sanchez@gmail.com>
2020-05-01 10:43:33 +0100

    Addition of timing code
    need to implement properly

--- Sorting algorithms/Quick sort.py
+++ Sorting algorithms/Quick sort.py
@@ -1,3 +1,5 @@
+from random import randint
+from timeit import repeat
 ''' Python implementation of QuickSort using Hoare's  
 partition scheme. '''
   
@@ -6,6 +8,11 @@ the pivot element at its correct position in sorted
     array, and places all smaller (smaller than pivot)  
 to left of pivot and all greater elements to right  
 of pivot '''
+
+# Driver code  
+arr = [6,5,2,77,55,334,53,553,53,64,76,24,86,43,55,1,335,545,6,7,38] 
+n = len(arr) 
+
 def partition(arr, low, high): 
       
     pivot = arr[low]  
@@ -54,10 +61,35 @@ def printArray(arr, n):
     for i in range(n): 
         print(arr[i],end=" ")  
     print()  
-  
-# Driver code  
-arr = [6,5,2,77,55,334,53,553,53,64,76,24,86,43,55,1,335,545,6,7,38] 
-n = len(arr) 
+
+
+def run_sorting_algorithm(algorithm, array):
+    # Set up the context and prepare the call to the specified
+    # algorithm using the supplied array. Only import the
+    # algorithm function if it's not the built-in `sorted()`.
+    setup_code = f"from __main__ import {algorithm}" \
+        if algorithm != "sorted" else ""
+
+    stmt = f"{algorithm}({array})"
+
+    # Execute the code ten different times and return the time
+    # in seconds that each execution took
+    times = repeat(setup=setup_code, stmt=stmt, repeat=3, number=10)
+
+    # Finally, display the name of the algorithm and the
+    # minimum time it took to run
+    print(f"Algorithm: {algorithm}. Minimum execution time: {min(times)}")
+
+
+if __name__ == "__main__":
+    # Generate an array of `ARRAY_LENGTH` items consisting
+    # of random integer values between 0 and 999
+    array = [randint(0, n) for i in range(n-1)]
+
+    # Call the function using the name of the sorting algorithm
+    # and the array you just created
+    run_sorting_algorithm(algorithm="quickSort", array=array)
+    
 quickSort(arr, 0, n - 1)  
 print("Sorted array:")  
 printArray(arr, n) 
